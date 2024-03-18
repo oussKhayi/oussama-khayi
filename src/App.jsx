eef645f33d44e894bafae420c9d403569546b779
ousskhayi <khayi.ouss@gmail.com>
2024-03-18 12:25:46 +0000

    some responsive design modifications and adding mobile nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,66 @@
 import React from "react";
 import { LeftSide } from "./components/LeftSide";
 import RightSide from "./components/RightSide";
-import { Nav } from "./components/Nav";
+import {
+  GrAppsRounded,
+  GrBriefcase,
+  GrContact,
+  GrDeploy,
+  GrUser,
+} from "react-icons/gr";
 
-export const App2 = () => {
+import "./App.css";
+
+const App = () => {
   return (
     <>
-      <div className="bg-primary h-auto lg:h-screen p-3 md:p-8 md:px-20 grid grid-rows-1 md:grid-cols-1 lg:grid-cols-2 md:overflow-hidden">
+      <div className="bg-primary h-auto lg:h-screen p-3 md:p-8 md:px-20 grid grid-rows-1 md:grid-cols-1 lg:grid-cols-2 md:overflow-hidden pb-12">
         <LeftSide />
         <RightSide />
+      </div>
+      <div className="fixed w-full bottom-0 lg:hidden bg-primary border-t-[0.01px] border-t-secondary bg-opacity-70 text-gray-400 pt-1">
+        <ul className="p-2 flex justify-around">
+          <li className="cursor-pointer hover:text-white hover:scale-110">
+            <a
+              href="#about"
+              className="cursor-pointer hover:text-white hover:scale-110"
+            >
+              <GrUser className="text-xl" />
+            </a>
+          </li>
+          <li className="cursor-pointer hover:text-white hover:scale-110">
+            <a
+              href="#experience"
+              className="cursor-pointer hover:text-white hover:scale-110"
+            >
+              <GrBriefcase className="text-xl" />
+            </a>
+          </li>
+          <li className="cursor-pointer hover:text-white hover:scale-110">
+            <a
+              href="#skills"
+              className="cursor-pointer hover:text-white hover:scale-110"
+            >
+              <GrDeploy className="text-xl" />
+            </a>
+          </li>
+          <li className="cursor-pointer hover:text-white hover:scale-110">
+            <a
+              href="#projects"
+              className="cursor-pointer hover:text-white hover:scale-110"
+            >
+              <GrAppsRounded className="text-xl" />
+            </a>
+          </li>
+          <li className="cursor-pointer hover:text-white hover:scale-110">
+            <a href="#contact" className="cursor-pointer">
+              <GrContact className="text-xl" />
+            </a>
+          </li>
+        </ul>
       </div>
     </>
   );
 };
+
+export default App;
--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import data from "../data.json";
 import { BsGithub, BsEye } from "react-icons/bs";
+import { ContactForm } from "./ContactForm";
 const RightSide = () => {
   return (
     <div className="w-full no-scroll-bar lg:pt-8 px-2 overflow-y-scroll no-scrollbar right-menu">
@@ -37,7 +38,10 @@ const RightSide = () => {
                 {/*  */}
                 {data.experience.map((exp, index) => {
                   return (
-                    <div className="relative group overflow-hidden p-4 md:p-8 rounded-xl border border-gray-800 bg-primary w-full">
+                    <div
+                      className="relative group overflow-hidden p-4 md:p-8 rounded-xl border border-gray-800 bg-primary w-full"
+                      key={index}
+                    >
                       <div
                         aria-hidden="true"
                         className="inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-blue-500 to-white dark:from-white dark:to-white blur-2xl opacity-25 dark:opacity-5 dark:group-hover:opacity-10"
@@ -66,7 +70,7 @@ const RightSide = () => {
           </div>
         </section>
       </div>
-      <div className="pe-16 text-gray-400 py-8" id="skills">
+      <div className="md:pe-16 text-gray-400 py-8" id="skills">
         <h3 className="text-2xl font-bold capitalize text-[#c4d0ec] hover:text-[#54d2d4] cursor-pointer">
           my skills
         </h3>
@@ -76,28 +80,12 @@ const RightSide = () => {
               <div className="grid  sm:grid-cols-1 gap-3 md:grid-cols-1 text-thirdly">
                 {/*  */}
                 Frontend
-                <div className="relative group overflow-hidden lg:px-8  p-4 rounded-xl border  border-gray-800 bg-primary grid gap-y-3 grid-cols-4 gap-x-3 md:grid-cols-5 lg:grid-cp">
+                <div className="relative group overflow-hidden lg:px-8  p-4 rounded-xl border  border-gray-800 bg-primary grid gap-y-3  grid-cols-4 gap-x-3 md:grid-cols-6  lg:grid-cols-5">
                   <div
                     aria-hidden="true"
                     className="inset-0 absolute border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-white to-white blur-2xl opacity-5 group-hover:opacity-10"
                   />
                   {data.technologies["front-end"].map((tech, index) => {
-                    return (
-                      <div className="bg-slate-800 start-16 border border-white/15 w-fit p-4 rounded-xl before:shadow-gray-950 ">
-                        <i className={`${tech} text-3xl`}></i>
-                      </div>
-                      // <div className="border border-blue-500/10 flex relative *:relative *:size-6 *:m-auto size-20 rounded-lg dark:bg-gray-900 dark:border-white/15 before:rounded-[7px] before:absolute before:inset-0 before:border-t before:border-white before:from-blue-100 dark:before:border-white/20 before:bg-gradient-to-b dark:before:from-white/10 dark:before:to-transparent before:shadow dark:before:shadow-gray-950">
-                      // </div>
-                    );
-                  })}
-                </div>
-                Backend
-                <div className="relative group overflow-hidden lg:px-8  p-4 rounded-xl border  border-gray-800 bg-primary grid gap-y-3 grid-cols-4 gap-x-3 md:grid-cols-6">
-                  <div
-                    aria-hidden="true"
-                    className="inset-0 absolute border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-white to-white blur-2xl opacity-5 group-hover:opacity-10"
-                  />
-                  {data.technologies["back-end"].map((tech, index) => {
                     return (
                       <div
                         className="bg-slate-800 start-16 border border-white/15 w-fit p-4 rounded-xl before:shadow-gray-950 "
@@ -110,9 +98,28 @@ const RightSide = () => {
                     );
                   })}
                 </div>
+                Backend
+                <div className="relative group overflow-hidden lg:px-8  p-4 rounded-xl border  border-gray-800 bg-primary grid gap-y-3  grid-cols-4 gap-x-3 md:grid-cols-6  lg:grid-cols-5">
+                  <div
+                    aria-hidden="true"
+                    className="inset-0 absolute border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-white to-white blur-2xl opacity-5 group-hover:opacity-10"
+                  />
+                  {data.technologies["back-end"].map((tech, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="bg-slate-800 start-16 border border-white/15 w-fit p-4 rounded-xl before:shadow-gray-950 "
+                      >
+                        <i className={`${tech} text-3xl`}></i>
+                      </div>
+                      // <div className="border border-blue-500/10 flex relative *:relative *:size-6 *:m-auto size-20 rounded-lg dark:bg-gray-900 dark:border-white/15 before:rounded-[7px] before:absolute before:inset-0 before:border-t before:border-white before:from-blue-100 dark:before:border-white/20 before:bg-gradient-to-b dark:before:from-white/10 dark:before:to-transparent before:shadow dark:before:shadow-gray-950">
+                      // </div>
+                    );
+                  })}
+                </div>
                 {/*  */}
                 Tools
-                <div className="relative group overflow-hidden lg:px-8  p-4 rounded-xl border  border-gray-800 bg-primary grid gap-y-3 grid-cols-4 gap-x-3 md:grid-cols-6">
+                <div className="relative group overflow-hidden lg:px-8  p-4 rounded-xl border  border-gray-800 bg-primary grid gap-y-3  grid-cols-4 gap-x-3 md:grid-cols-6  lg:grid-cols-5">
                   <div
                     aria-hidden="true"
                     className="inset-0 absolute border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-white to-white blur-2xl opacity-5 group-hover:opacity-10"
@@ -120,8 +127,8 @@ const RightSide = () => {
                   {data.technologies["tools"].map((tech, index) => {
                     return (
                       <div
-                        className="bg-slate-800 start-16 border border-white/15 w-fit p-4 rounded-xl before:shadow-gray-950 "
                         key={index}
+                        className="bg-slate-800 start-16 border border-white/15 w-fit p-4 rounded-xl before:shadow-gray-950 "
                       >
                         <i className={`${tech} text-3xl`}></i>
                       </div>
@@ -138,7 +145,7 @@ const RightSide = () => {
       </div>
       {/*  */}
       {/*  */}
-      <div className="pe-16 text-gray-400 py-8" id="projects">
+      <div className="md:pe-16 text-gray-400 py-8" id="projects">
         <h3 className="text-2xl font-bold capitalize text-[#c4d0ec] hover:text-[#54d2d4] cursor-pointer">
           my projects
         </h3>
@@ -149,7 +156,10 @@ const RightSide = () => {
                 {/*  */}
                 {data.projects.map((prd, index) => {
                   return (
-                    <div className="relative group overflow-hidden p-8 rounded-xl border  border-gray-800 bg-primary">
+                    <div
+                      key={index}
+                      className="relative group overflow-hidden p-8 rounded-xl border  border-gray-800 bg-primary"
+                    >
                       <div
                         aria-hidden="true"
                         className="inset-0 absolute aspect-video border rounded-full -translate-y-1/2 group-hover:-translate-y-1/4 duration-300 bg-gradient-to-b from-blue-500 to-white dark:from-white dark:to-white blur-2xl opacity-25 dark:opacity-5 dark:group-hover:opacity-10"
@@ -209,7 +219,7 @@ const RightSide = () => {
                             {prd.title}
                           </h4>
                           <div className="mt-2 pb-2">
-                            <p className="text-gray-400 group-hover:text-gray-300 text-xs max-w-[75%] md:max-w-auto">
+                            <p className="text-gray-400 group-hover:text-gray-300 text-xs max-w-[70%] md:max-w-[80%] md:max-w-auto">
                               {prd.description}
                             </p>
                           </div>
@@ -236,6 +246,26 @@ const RightSide = () => {
           </div>
         </section>
       </div>
+      {/*  */}
+      {/*  */}
+      <div className="md:pe-16 text-gray-400 py-8" id="contact">
+        <h3 className="text-2xl font-bold capitalize text-[#c4d0ec] hover:text-[#54d2d4] cursor-pointer">
+          Contact me
+        </h3>
+        <section className="relative top-4">
+          <div>
+            <div className="mx-auto max-w-6xl text-gray-500">
+              <div className="grid gap-3">
+                {/*  */}
+                <ContactForm />
+                {/*  */}
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+      {/*  */}
+      {/*  */}
     </div>
   );
 };
